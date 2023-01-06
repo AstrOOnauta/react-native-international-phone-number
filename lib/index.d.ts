405052315f3c179aeb5c0a604e6a79b420374a61
Will <willian.ralf.rn@gmail.com>
2023-01-06 08:33:23 -0300

    fix: inputStyle types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,17 @@
 import { Dispatch, SetStateAction } from 'react';
-import { StyleProp, TextInputProps, ViewStyle } from 'react-native';
+import {
+  StyleProp,
+  TextInputProps,
+  TextStyle,
+  ViewStyle,
+} from 'react-native';
 import { Country } from 'react-native-country-picker-modal';
 
 interface PhoneInputProps extends TextInputProps {
   placeholder?: string;
   placeholderTextColor?: string;
   containerStyle?: StyleProp<ViewStyle>;
-  inputStyle?: StyleProp<ViewStyle>;
+  inputStyle?: StyleProp<TextStyle>;
   withDarkTheme?: boolean;
   disabled?: boolean;
   selectedCountry: undefined | Country;

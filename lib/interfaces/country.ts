7d3de49b9307c1b1f642f4653c817eac1a8063d4
Will <willian.ralf.rn@gmail.com>
2023-07-16 09:23:09 -0300

    refactor: lib types

---
+++ lib/interfaces/country.ts
@@ -0,0 +1,6 @@
+export interface ICountry {
+  callingCode: string;
+  cca2: string;
+  flag: string;
+  name: string;
+}
---
+++ lib/interfaces/phoneInputProps.ts
@@ -0,0 +1,23 @@
+import {
+  StyleProp,
+  TextInputProps,
+  TextStyle,
+  ViewStyle,
+} from 'react-native';
+
+import { ICountry } from './country';
+
+export interface PhoneInputProps extends TextInputProps {
+  placeholder?: string;
+  placeholderTextColor?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+  flagContainerStyle?: StyleProp<ViewStyle>;
+  inputStyle?: StyleProp<TextStyle>;
+  withDarkTheme?: boolean;
+  disabled?: boolean;
+  defaultValue?: string;
+  value: string;
+  onChangePhoneNumber: (phoneNumber: string) => void;
+  selectedCountry: undefined | ICountry;
+  onChangeSelectedCountry: (country: ICountry) => void;
+}

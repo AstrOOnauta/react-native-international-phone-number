d4f6931813e9a3a3b0a02c1ffb9eb5d971bca23a
Will <barbosa.will.1998@gmail.com>
2025-11-01 13:46:43 -0300

    feat: enhance PhoneInputRef with additional TextInput methods

--- lib/interfaces/phoneInputRef.ts
+++ lib/interfaces/phoneInputRef.ts
@@ -1,12 +1,45 @@
-import {TextInput} from 'react-native';
-import {ICountry} from 'react-native-country-select';
+import { TextInput } from 'react-native';
+import { ICountry } from 'react-native-country-select';
 
-import {PhoneInputProps} from './phoneInputProps';
+import { PhoneInputProps } from './phoneInputProps';
 
 export interface IPhoneInputRef extends TextInput {
   props: PhoneInputProps;
+  // Métodos nativos do TextInput
   onFocus: () => void;
   focus: () => void;
+  blur: () => void;
+  clear: () => void;
+  isFocused: () => boolean;
+  setNativeProps: (nativeProps: object) => void;
+  measure: (
+    callback: (
+      x: number,
+      y: number,
+      width: number,
+      height: number,
+      pageX: number,
+      pageY: number
+    ) => void
+  ) => void;
+  measureInWindow: (
+    callback: (
+      x: number,
+      y: number,
+      width: number,
+      height: number
+    ) => void
+  ) => void;
+  measureLayout: (
+    relativeToNativeNode: number,
+    onSuccess: (
+      x: number,
+      y: number,
+      width: number,
+      height: number
+    ) => void,
+    onFail: () => void
+  ) => void;
   getValue: () => string;
   value: string;
   getValueFormatted: () => string;
